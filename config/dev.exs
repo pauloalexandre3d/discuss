c07c4c6244a5dd59305757b0f88ec638b950c998
Paulinho <pauloalexandre3d@gmail.com>
2019-02-04 18:39:50 -0200

    Adding changes layout pages

--- config/dev.exs
+++ config/dev.exs
@@ -49,7 +49,7 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "paulo",
+  username: "pauloalexandre3d",
   password: "",
   database: "discuss_dev",
   hostname: "localhost",
